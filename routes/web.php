26eeb9dff6315bd2878d9dbf2cd69696c833873e
Sergio R. González <sergio@comovas.com>
2025-05-24 14:10:45 +0200

    Actualizar comentario en la ruta de la API para mayor claridad

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PruebaController;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-//APIS
+//APIS TEST CAMBIO
 Route::apiResource('api/pruebas', PruebaController::class);
